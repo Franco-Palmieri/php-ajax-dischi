cc65010c91427e4916ba29896fee16d7b9d5d6fb
Ncesecrede <francescopm95@gmail.com>
2021-08-30 15:06:51 +0100

    Ajax Dischi

--- primamilestone.php
+++ primamilestone.php
@@ -5,17 +5,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="./style/style.css">
 </head>
 <body>
 
-    <?php
+    <nav></nav>
+    <main class="container">
+        <div class="row">
+            <?php
+                require_once __DIR__ . "/database/database.php";
 
-        require_once __DIR__ . "/database/database.php";
-
-        foreach($database as $detail){
-            echo "<div> {$detail['title']} </div>";
-        }
-
-    ?>
+                foreach($database as $detail){
+                    echo "<div class='cosa'> <div class='album'> {$detail['title']} </div> </div>";
+                }
+            ?>
+        </div>        
+            </main>
+    
 </body>
 </html>
