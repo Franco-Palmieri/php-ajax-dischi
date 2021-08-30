7e5156b784ee46a19ad5e1a37731d13bbc1862a5
Ncesecrede <francescopm95@gmail.com>
2021-08-30 13:44:40 +0100

    Ajax Dischi

--- script/script.js
+++ script/script.js
@@ -3,9 +3,12 @@ const app = new Vue(
     {
         el: '#app',
         data: {
+            newArray: [],
         },
         created(){
-            
+            axios.get('http://localhost/Corso/Esercizio4PHP/php-ajax-dischi/database/api.php').then((response) =>{
+                this.newArray = response.data;
+            })
         },
         methods: {
         },
